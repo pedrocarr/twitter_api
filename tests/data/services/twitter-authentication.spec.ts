83620fe5e4379d2b288459bf33408ee7938cd0f1
pedrocarr <eng.pedro.amb@gmail.com>
2022-11-17 14:20:53 -0300

    simplify code

--- tests/data/services/twitter-authentication.spec.ts
+++ tests/data/services/twitter-authentication.spec.ts
@@ -6,22 +6,23 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('TwitterAuthenticationService', () => {
   let loadTwitterUserApi: MockProxy<LoadTwitterUserApi>
   let sut: TwitterAuthenticationService
+  const token = 'any_token'
 
   beforeEach(() => {
     loadTwitterUserApi = mock()
     sut = new TwitterAuthenticationService(loadTwitterUserApi)
   })
   it('should call LoadTwitterUserApi with the correct parameters', async () => {
-    await sut.perform({ token: 'any_token' })
+    await sut.perform({ token })
 
-    expect(loadTwitterUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadTwitterUserApi.loadUser).toHaveBeenCalledWith({ token })
     expect(loadTwitterUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadTwitterUserApi returns undefined', async () => {
     loadTwitterUserApi.loadUser.mockResolvedValueOnce(undefined)
 
-    const authResult = await sut.perform({ token: 'any_token' })
+    const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
