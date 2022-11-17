47462bf5878e3b6bb674bb69d7ded44caa390b02
pedrocarr <eng.pedro.amb@gmail.com>
2022-11-17 13:40:04 -0300

    ensure Service returns AuthenticationError when LoadTwitterUserApi returns undefined

--- tests/data/services/twitter-authentication.spec.ts
+++ tests/data/services/twitter-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { AuthenticationError } from '@/domain/errors'
 import { TwitterAuthentication } from '@/domain/features'
 
 class TwitterAuthenticationService {
@@ -5,25 +6,30 @@ class TwitterAuthenticationService {
     private readonly loadTwitterUserApi: LoadTwitterUserApi
   ) {}
 
-  async perform (params: TwitterAuthentication.Params): Promise<void> {
+  async perform (params: TwitterAuthentication.Params): Promise<AuthenticationError> {
     await this.loadTwitterUserApi.loadUser(params)
+    return new AuthenticationError()
   }
 }
 
 interface LoadTwitterUserApi {
-  loadUser: (params: LoadTwitterUserApi.Params) => Promise<void>
+  loadUser: (params: LoadTwitterUserApi.Params) => Promise<LoadTwitterUserApi.Result>
 }
 
 namespace LoadTwitterUserApi {
   export type Params = {
     token: string
   }
+
+  export type Result = undefined
 }
 
 class LoadTwitterUserApiSpy implements LoadTwitterUserApi {
   token?: string
-  async loadUser (params: LoadTwitterUserApi.Params): Promise<void> {
+  result = undefined
+  async loadUser (params: LoadTwitterUserApi.Params): Promise<LoadTwitterUserApi.Result> {
     this.token = params.token
+    return this.result
   }
 }
 
@@ -31,9 +37,18 @@ describe('TwitterAuthenticationService', () => {
   it('should call LoadTwitterUserApi with the correct parameters', async () => {
     const loadTwitterUserApi = new LoadTwitterUserApiSpy()
     const sut = new TwitterAuthenticationService(loadTwitterUserApi)
-
     await sut.perform({ token: 'any_token' })
 
     expect(loadTwitterUserApi.token).toBe('any_token')
+  })
+
+  it('should return AuthenticationError when LoadTwitterUserApi returns undefined', async () => {
+    const loadTwitterUserApi = new LoadTwitterUserApiSpy()
+    loadTwitterUserApi.result = undefined
+    const sut = new TwitterAuthenticationService(loadTwitterUserApi)
+
+    const authResult = await sut.perform({ token: 'any_token' })
+
+    expect(authResult).toEqual(new AuthenticationError())
   })
 })
