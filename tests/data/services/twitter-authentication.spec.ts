08076fd180cee0e862c732f9b33c0ac7c1fa32f7
pedrocarr <eng.pedro.amb@gmail.com>
2022-11-17 14:06:27 -0300

    change test strategy to use jest mock extended

--- tests/data/services/twitter-authentication.spec.ts
+++ tests/data/services/twitter-authentication.spec.ts
@@ -1,11 +1,11 @@
 import { AuthenticationError } from '@/domain/errors'
 import { TwitterAuthenticationService } from '@/data/services'
+import { LoadTwitterUserApi } from '@/data/contracts/apis'
+import { mock } from 'jest-mock-extended'
 
 describe('TwitterAuthenticationService', () => {
   it('should call LoadTwitterUserApi with the correct parameters', async () => {
-    const loadTwitterUserApi = {
-      loadUser: jest.fn()
-    }
+    const loadTwitterUserApi = mock<LoadTwitterUserApi>()
     const sut = new TwitterAuthenticationService(loadTwitterUserApi)
     await sut.perform({ token: 'any_token' })
 
@@ -14,9 +14,7 @@ describe('TwitterAuthenticationService', () => {
   })
 
   it('should return AuthenticationError when LoadTwitterUserApi returns undefined', async () => {
-    const loadTwitterUserApi = {
-      loadUser: jest.fn()
-    }
+    const loadTwitterUserApi = mock<LoadTwitterUserApi>()
     loadTwitterUserApi.loadUser.mockResolvedValueOnce(undefined)
     const sut = new TwitterAuthenticationService(loadTwitterUserApi)
 
