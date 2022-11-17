4990a9a0a9f439a60f9cccee7450307b37218e35
pedrocarr <eng.pedro.amb@gmail.com>
2022-11-17 14:02:33 -0300

    change test strategy to use jest mocks

--- tests/data/services/twitter-authentication.spec.ts
+++ tests/data/services/twitter-authentication.spec.ts
@@ -1,31 +1,23 @@
 import { AuthenticationError } from '@/domain/errors'
-import { LoadTwitterUserApi } from '@/data/contracts/apis'
 import { TwitterAuthenticationService } from '@/data/services'
-
-class LoadTwitterUserApiSpy implements LoadTwitterUserApi {
-  token?: string
-  callsCount = 0
-  result = undefined
-  async loadUser (params: LoadTwitterUserApi.Params): Promise<LoadTwitterUserApi.Result> {
-    this.token = params.token
-    this.callsCount++
-    return this.result
-  }
-}
 
 describe('TwitterAuthenticationService', () => {
   it('should call LoadTwitterUserApi with the correct parameters', async () => {
-    const loadTwitterUserApi = new LoadTwitterUserApiSpy()
+    const loadTwitterUserApi = {
+      loadUser: jest.fn()
+    }
     const sut = new TwitterAuthenticationService(loadTwitterUserApi)
     await sut.perform({ token: 'any_token' })
 
-    expect(loadTwitterUserApi.token).toBe('any_token')
-    expect(loadTwitterUserApi.callsCount).toBe(1)
+    expect(loadTwitterUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadTwitterUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadTwitterUserApi returns undefined', async () => {
-    const loadTwitterUserApi = new LoadTwitterUserApiSpy()
-    loadTwitterUserApi.result = undefined
+    const loadTwitterUserApi = {
+      loadUser: jest.fn()
+    }
+    loadTwitterUserApi.loadUser.mockResolvedValueOnce(undefined)
     const sut = new TwitterAuthenticationService(loadTwitterUserApi)
 
     const authResult = await sut.perform({ token: 'any_token' })
