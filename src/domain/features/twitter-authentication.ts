75a3e237c4422c17d132f5f9921acdf941d77ea3
pedrocarr <eng.pedro.amb@gmail.com>
2022-11-17 10:22:28 -0300

    add twitter authentication feature

---
+++ src/domain/features/twitter-authentication.ts
@@ -0,0 +1,14 @@
+import { AuthenticationError } from '@/domain/errors'
+import { AccessToken } from '@/domain/models'
+
+export interface TwitterAuthentication {
+  perform: (params: TwitterAuthentication.Params) => Promise<TwitterAuthentication.Result>
+}
+
+namespace TwitterAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
