e48a4141a1e3d1618c0831c1dc249b9d08ba253b
pedrocarr <eng.pedro.amb@gmail.com>
2022-12-01 09:36:16 -0300

    ensure TwitterApi get app token with correct params

--- tests/infra/apis/twitter.spec.ts
+++ tests/infra/apis/twitter.spec.ts
@@ -4,10 +4,21 @@ import { mock } from 'jest-mock-extended'
 class TwitterApi {
   private readonly baseUrl = 'https://api.twitter.com'
 
-  constructor (private readonly httpClient: HttpGetClient) {}
+  constructor (
+    private readonly httpClient: HttpGetClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string
+  ) {}
 
   async loadUser (params: LoadTwitterUserApi.Params): Promise<void> {
-    await this.httpClient.get({ url: `${this.baseUrl}/oauth/authorize` })
+    await this.httpClient.get({
+      url: `${this.baseUrl}/oauth/authorize`,
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: 'client_credentials'
+      }
+    })
   }
 }
 
@@ -18,18 +29,26 @@ interface HttpGetClient {
 namespace HttpGetClient {
   export type Params = {
     url: string
+    params: object
   }
 }
 
 describe('TwitterApi', () => {
+  const clientId = 'any_client_id'
+  const clientSecret = 'any_client_secret'
   it('should get app token', async () => {
     const httpClient = mock<HttpGetClient>()
-    const sut = new TwitterApi(httpClient)
+    const sut = new TwitterApi(httpClient, clientId, clientSecret)
 
     await sut.loadUser({ token: 'any_client_token' })
 
     expect(httpClient.get).toHaveBeenCalledWith({
-      url: 'https://api.twitter.com/oauth/authorize'
+      url: 'https://api.twitter.com/oauth/authorize',
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials'
+      }
     })
   })
 })
